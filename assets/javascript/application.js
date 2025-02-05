ce92c8e4082acceb6db2b70af6cf1750a3fc4d1b
Dks2006-crypto <denis18kpina@gmail.com>
2025-02-05 23:27:39 +0300

    Сделал секцию our work, подключил слайдер

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,1 +1,10 @@
-console.log('ok');
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+});
